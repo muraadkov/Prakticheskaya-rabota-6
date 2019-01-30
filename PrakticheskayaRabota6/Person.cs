dd2e453adfeada62a3554d769268be647d66dd85
Адылканов Муратжан Талгатович <@corp.itstep>
2019-01-30 21:17:14 +0600

    Сделал шестую практическую работу

---
+++ PrakticheskayaRabota6/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Surname { get; set; }
+        public static bool operator==(Person obj1, Person obj2)
+        {
+            return obj1.Age == obj2.Age;
+        }
+        public static bool operator!=(Person obj1,Person obj2)
+        {
+            return obj1.Age != obj2.Age;
+        }
+    }
+}
---
+++ PrakticheskayaRabota6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person
+            {
+                Name = "Петя",
+                Age = 15
+            };
+            Person secondPerson = new Person
+            {
+                Name = "Петя",
+                Age = 28
+        };
+            Console.WriteLine("Возраст первого человека: " + firstPerson.Age);
+            Console.WriteLine("Имя первого человека: " + firstPerson.Name);
+            Console.WriteLine("Возраст второго человека: " + secondPerson.Age);
+            Console.WriteLine("Имя второго человека: " + secondPerson.Name);
+            Console.WriteLine("--------------------------------------------------------");
+            Console.Write("Одинаковое имя: ");
+            Console.WriteLine(firstPerson.Name == secondPerson.Name);
+            Console.Write("Разное имя: ");
+            Console.WriteLine(firstPerson.Name != secondPerson.Name);
+            Console.Write("Одинаковый возраст: ");
+            Console.WriteLine(firstPerson.Age == secondPerson.Age);
+            Console.Write("Разный возраст: ");
+            Console.WriteLine(firstPerson.Age != secondPerson.Age);
+            Console.Read();
+        }
+    }
+}
